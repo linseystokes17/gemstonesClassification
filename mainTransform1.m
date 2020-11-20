c5efa58067d0f0ec3c06c3c403c3028539b8e120
Linsey Stokes <linseystokes17@gmail.com>
2020-11-20 15:56:14 -0600

    initial commit. Contains images and starter code)

---
+++ mainTransform1.m
@@ -0,0 +1,7 @@
+topaz1 = imread("gemstones/train/Topaz/topaz_0.jpg");
+topaz2 = imread("gemstones/train/Topaz/topaz_1.jpg");
+subplot(1, 2, 1), imshow(topaz1);
+subplot(1, 2, 2), imshow(topaz2);
+imshow(topaz2);
+pause;
+close;
