c97313fd8f8478a81e20f28a531a5ad6fbabe807
Linsey Stokes <linseystokes17@gmail.com>
2020-11-20 17:04:07 -0600

    did a little edge testing, need some image cleanup before edges but looks viable

--- mainTransform1.m
+++ mainTransform1.m
@@ -1,7 +1,33 @@
 topaz1 = imread("gemstones/train/Topaz/topaz_0.jpg");
 topaz2 = imread("gemstones/train/Topaz/topaz_1.jpg");
-subplot(1, 2, 1), imshow(topaz1);
-subplot(1, 2, 2), imshow(topaz2);
-imshow(topaz2);
+ruby1 = imread("gemstones/train/ruby/ruby_0.jpg");
+ruby2 = imread("gemstones/train/ruby/ruby_1.jpg");
+moonstone1 = imread("gemstones/train/moonstone/moonstone_0.jpg");
+
+topaz1Canny = edge(rgb2gray(topaz1), 'canny');
+topaz2Canny = edge(rgb2gray(topaz2), 'canny');
+ruby1Canny = edge(rgb2gray(ruby1), 'canny');
+ruby2Canny = edge(rgb2gray(ruby2), 'canny');
+moonstone1Canny = edge(rgb2gray(moonstone1), 'canny');
+
+
+subplot(2, 2, 1), imshow(topaz1Canny);
+subplot(2, 2, 2), imshow(topaz2Canny);
+subplot(2, 2, 3), imshow(ruby1Canny);
+subplot(2, 2, 4), imshow(ruby2Canny);
+pause;
+
+moonstoneAdjusted = imagesc(moonstone1);
+
+figure('Name', 'Moonstone'), subplot(2, 2, 1),  imshow(moonstone1Canny);
+subplot(2,2,2), imshow(moonstone1);
+subplot(2,2,3), imshow(moonstoneAdjusted);
+
+pause;
+
+subplot(2, 2, 1), imshow(topaz1);
+subplot(2, 2, 2), imshow(topaz2);
+subplot(2, 2, 3), imshow(ruby1);
+subplot(2, 2, 4), imshow(ruby2);
 pause;
 close;
